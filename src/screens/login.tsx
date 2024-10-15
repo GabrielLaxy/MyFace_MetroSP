192867c7fb14e316551e1264dab4b0a63d263f16
Gabriel Laxy <gablaxycarreira@gmail.com>
2024-10-15 18:03:40 -0300

    interface login finalizada

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,59 +1,84 @@
-import React, { useState } from "react";
-import { View, Text, Image } from "react-native";
-import { TextInput, Button, IconButton } from "react-native-paper";
-import theme from "../styles/theme";
-import styles from "../styles/loginStyle";
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import { TextInput, Button, IconButton } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import theme from '../styles/theme';
+import styles from '../styles/loginStyle';
 
 const mascote = require('../../assets/Mascote-Metro-SP 3.png');
 const logo = require('../../assets/Metro logo.png');
 
 export default function Login() {
-  const [funcional, setFuncional] = useState("");
-  const [senha, setSenha] = useState("");
-  const [mostrarSenha, setMostrarSenha] = useState(false);
+	const [funcional, setFuncional] = useState('');
+	const [senha, setSenha] = useState('');
+	const [mostrarSenha, setMostrarSenha] = useState(false);
 
-  const handleLogin = () => {
-    console.log("Funcional:", funcional);
-    console.log("Senha:", senha);
-  };
+	const handleLogin = () => {
+		console.log('Funcional:', funcional);
+		console.log('Senha:', senha);
+	};
 
-  return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.logo}/>
-      <Image source={mascote} style={styles.mascote}/>
-      <Text style={styles.title}>Entre na sua conta</Text>
-      <Text style={styles.subtitle}>Bem vindo ao sistema!</Text>
-      <TextInput
-        label="Funcional"
-        value={funcional}
-        onChangeText={(text) => setFuncional(text)}
-        style={styles.input}
-        mode="outlined"
-        theme={theme}
-      />
-      <TextInput
-        label="Senha"
-        value={senha}
-        onChangeText={(text) => setSenha(text)}
-        style={styles.input}
-        secureTextEntry={!mostrarSenha}
-        mode="outlined"
-        right={
-          <TextInput.Icon
-            icon={mostrarSenha ? "eye-off" : "eye"}
-            onPress={() => setMostrarSenha(!mostrarSenha)}
-          />
-        }
-        theme={theme}
-      />
-      <Button
-        mode="contained"
-        onPress={handleLogin}
-        style={styles.button}
-        labelStyle={styles.buttonLabel}
-      >
-        Entrar
-      </Button>
-    </View>
-  );
+	const handleMailto = (emailSubject: string) => {
+		Linking.openURL(`mailto:exemplo@email.com?subject=${emailSubject}`);
+	};
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<StatusBar animated={true} style="auto" />
+			<View style={styles.logoContainer}>
+				<Image source={logo} style={styles.logo} />
+			</View>
+			<View style={styles.mascotContainer}>
+				<Image source={mascote} style={styles.mascote} />
+			</View>
+			<View style={styles.middleContainer}>
+				<View style={styles.textContainer}>
+					<Text style={styles.title}>Entre na sua conta</Text>
+					<Text style={styles.subtitle}>Bem vindo ao sistema!</Text>
+				</View>
+				<TextInput
+					label="Funcional"
+					value={funcional}
+					onChangeText={text => setFuncional(text)}
+					style={styles.input}
+					mode="outlined"
+					theme={theme}
+				/>
+				<TextInput
+					label="Senha"
+					value={senha}
+					onChangeText={text => setSenha(text)}
+					style={styles.input}
+					secureTextEntry={!mostrarSenha}
+					mode="outlined"
+					right={
+						<TextInput.Icon
+							icon={mostrarSenha ? 'eye-off' : 'eye'}
+							onPress={() => setMostrarSenha(!mostrarSenha)}
+						/>
+					}
+					theme={theme}
+				/>
+				<TouchableOpacity onPress={() => handleMailto('Esqueci minha senha')}>
+					<Text style={styles.forgotPw}>Esqueci minha senha</Text>
+				</TouchableOpacity>
+				<Button
+					mode="contained"
+					onPress={handleLogin}
+					style={styles.button}
+					labelStyle={styles.buttonLabel}
+				>
+					Entrar
+				</Button>
+			</View>
+			<View style={styles.bottomContainer}>
+				<TouchableOpacity
+					onPress={() => handleMailto('Política e termos de privacidade')}
+				>
+					<Text style={styles.link}>Política e termos de privacidade</Text>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	);
 }
--- src/styles/loginStyle.ts
+++ src/styles/loginStyle.ts
@@ -1,44 +1,77 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 import theme from './theme';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent:'center',
-    padding: 16,
-  },
-  title: {
-    marginBottom: 4,
-    fontSize:30,
-    textAlign: "center",
-    fontWeight:'bold',
-  },
-  subtitle:{
-    margin:2,
-    fontSize:15,
-    textAlign: "center",
-  },
-  input: {
-    marginBottom: 16,
-  },
-  button: {
-    marginTop: 12,
-    backgroundColor:theme.colors.accent,
-  },
-  buttonLabel:{
-    color:theme.colors.text,
-    fontWeight:'bold',
-  },
-  logo:{
-    width:1000.49/5,
-    height:263.51/5,
-    alignSelf:'center',
-  },
-  mascote:{
-    width:120,
-    height:120,
-    alignSelf:'center',
-  }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		paddingVertical: 16,
+		paddingHorizontal: 22,
+		backgroundColor: 'white',
+	},
+	logoContainer: {
+		flex: 1,
+		justifyContent: 'flex-start',
+	},
+	mascotContainer: {
+		flex: 2,
+		justifyContent: 'flex-end',
+	},
+	middleContainer: {
+		flex: 5,
+	},
+	textContainer: {
+		flex: 0.5,
+		justifyContent: 'center',
+	},
+	bottomContainer: {
+		flex: 0.5,
+		justifyContent: 'flex-end',
+	},
+	title: {
+		marginBottom: 4,
+		fontSize: 36,
+		textAlign: 'center',
+		fontWeight: 'bold',
+	},
+	subtitle: {
+		margin: 2,
+		fontSize: 16,
+		textAlign: 'center',
+	},
+	input: {
+		marginBottom: 16,
+	},
+	button: {
+		marginTop: 16,
+		backgroundColor: theme.colors.accent,
+	},
+	buttonLabel: {
+		color: theme.colors.text,
+		fontWeight: 'bold',
+	},
+	logo: {
+		width: 1000.49 / 6,
+		height: 263.51 / 6,
+		alignSelf: 'center',
+	},
+	mascote: {
+		width: 120,
+		height: 120,
+		alignSelf: 'center',
+	},
+	link: {
+		marginTop: 12,
+		textAlign: 'center',
+		textDecorationLine: 'underline',
+		fontSize: 12,
+	},
+	forgotPw: {
+		alignSelf: 'flex-start',
+		textAlign: 'center',
+		textDecorationLine: 'underline',
+		fontSize: 15,
+	},
 });
 
 export default styles;
